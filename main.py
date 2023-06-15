ec4e4cc8ee970d55160d5c909e655155e17c5622
Aydın Serhat Seyitoğlu <aydinserhatseyitoglu@gmail.com>
2023-06-15 12:13:13 +0300

    catch the turtle, count added

--- main.py
+++ main.py
@@ -7,16 +7,30 @@ board.bgcolor("Light Blue")
 board.title("Catch The Turtle")
 turtle.shape("turtle")
 turtle.shapesize(2)
+score = turtle.Turtle()
+sayı=0
+def count():
+    score.hideturtle()
+    score.penup()
+    score.setposition(0,300)
+    score.write(sayı, move=False, align='center',font=('Arial',13,'normal'))
+
 
 def atama(x,y):
-    x=random.randint(0,180)
-    y=random.randint(0,250)
+
+    x=random.randint(-250,300)
+    y=random.randint(-250,250)
     turtle.hideturtle()
     turtle.setpos(x,y)
     time.sleep(0.5)
     turtle.hideturtle()
     time.sleep(0.5)
     turtle.showturtle()
+    global sayı
+    sayı +=1
+    score.clear()
+    count()
+
 
 turtle.penup()
 turtle.hideturtle()
@@ -26,6 +40,6 @@ turtle.hideturtle()
 time.sleep(0.5)
 turtle.showturtle()
 
-turtle.onclick(atama, btn=1, add=True)
+turtle.onclick(atama, btn=1, add=False)
 
 board.mainloop()
